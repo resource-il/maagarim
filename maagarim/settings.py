056390ba5df0577bf0cc62ae14064b7b90a891f9
Yehuda Deutsch <yeh@uda.co.il>
2015-08-10 23:18:24 +0300

    fixed static files

--- maagarim/settings.py
+++ maagarim/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2h-19!i$zk-^7k0vaq$8w(z&56(nz^bp$n^=6#40sln7q99$ln'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
